929496f647faf770d99417c8c15ab26c1250e124
Joey <joseph.hopkins1095@gmail.com>
2024-01-08 13:11:22 -0700

    Did a basic setup of a nav bar for the site

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -2,11 +2,42 @@
   <v-app>
     <v-app-bar :elevation="2" color="primary">
       <v-app-bar-title color="black--text" class="title">Joey Hopkins</v-app-bar-title>
+      <template v-slot:extension>
+        <v-tabs
+        v-model="tab"
+        bg-color="transparent"
+        color="basil"
+        grow
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item"
+          :value="item"
+        >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+      </template>
+
     </v-app-bar>
   </v-app>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'About', 'Portfolio', 'Contact',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
+  }
+</script>
 <style scoped>
 .title { 
-  color: black;
+  color: white;
+  text-align: center;
 }
 </style>
