8dd560405f85d81b24b0701f2387efa1b1dc2916
Joey <joseph.hopkins1095@gmail.com>
2024-01-10 11:15:26 -0700

    Change router links to hook up to the name as opposed to the url location

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -2,9 +2,9 @@
   <v-container class="center-content">
     <div class="nav-container center-content-vertical">
       <div class="margin-horizontal-15 center-content spread">
-          <router-link class="nav-btn center-content" to="/"><div>Work</div></router-link>
-          <router-link class="nav-btn center-content" to="/about"><div>About</div></router-link>        
-          <router-link class="nav-btn center-content" to="/contact"><div>Contact</div></router-link>        
+          <router-link class="nav-btn center-content" :to="{ name: 'Home' }"><div>Work</div></router-link>
+          <router-link class="nav-btn center-content" :to="{ name: 'About' }"><div>About</div></router-link>        
+          <router-link class="nav-btn center-content" :to="{ name: 'Contact' }"><div>Contact</div></router-link>        
       </div>
     </div>
   </v-container>
