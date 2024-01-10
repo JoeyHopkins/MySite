b5dadc4b38895c1af520d834ec8cd5d3a756b251
Joey <joseph.hopkins1095@gmail.com>
2024-01-09 21:34:24 -0700

    Get a base setup of router buttons

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,6 +1,17 @@
 <template>
   <container class="mt-10 center-content">
-      <div class="nav-container">
+      <div class="nav-container center-content-vertical">
+        <div class="margin-horizontal-15 center-content spread">
+          <div class="nav-btn center-content">
+            Work
+          </div>
+          <div class="nav-btn center-content">
+            About
+          </div>
+          <div class="nav-btn center-content">
+            Contact
+          </div>
+        </div>
       </div>
   </container>
 </template>
