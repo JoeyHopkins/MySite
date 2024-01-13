517efc2554a4cab64e141562914c598ac9e80eda
Joey <joseph.hopkins1095@gmail.com>
2024-01-12 20:58:38 -0700

    Setup basic layout of work history and education on about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,154 @@
 <template>
-  <div>
-  Test About
-  </div>
+  <v-container class="center-content">
+    <div>
+      <h1 class="center-text text-blue">
+        <b>
+          Resume
+        </b>
+      </h1>
+      <div class="download-link" @click="openPdfInNewTab">
+        <span class="center-content">View PDF</span>
+      </div>
+    </div>
+  </v-container>
+  <v-container class="center-content">
+    <v-card class="page-card">
+      <h2 class="text-blue mx-5 py-3">
+        <b>
+          Experience
+        </b>
+      </h2>
+      <div style="margin-bottom: -20px;">
+        <img 
+          src="/src/assets/images/MacTechLogo.png" 
+          alt="Macguyvertech Logo" 
+          width="200" 
+          height="75"
+        >
+      </div>
+      <h4 class="mx-5 lighten-text">
+        January 2022 - PRESENT
+      </h4>
+      <h3 class="mx-5">
+        <b>
+          MacGuyverTech, Glenolden, PA (Remote) - 
+        </b>
+        <i>  
+          Lead Full Stack Engineer / Project Manager
+        </i>
+      </h3>
+      <ul class="bullet-points mx-5 pt-5">
+        <li class="bullet-list-padding">Integral piece to application development due to depth of knowledge and understanding of client systems</li>
+        <li class="bullet-list-padding">100% remote, trusted to work independently primarily using Node/Vue and SQL DBs to build business applications</li>
+        <li class="bullet-list-padding">Streamlined long term vision for project in project management role by establishing weekly sprints with client</li>
+        <li class="bullet-list-padding">Optimized test in production by leading architecture of production and software upgrades</li>
+        <li class="bullet-list-padding">Trusted to take decisive and accurate actions when conflicts arise</li>
+        <li class="bullet-list-padding">Forecasted and avoided blockers by organizing the team of developers and managing the project without incentive</li>
+        <li class="bullet-list-padding">Managed application upkeep and pushes to testing environments and production servers</li>
+        <li class="bullet-list-padding">Took Initiative in managing databases and writing scripts to refresh clients test data daily</li>
+        <li class="bullet-list-padding">Relied upon to revise and educate junior developers preventing sub par code from making it to production servers</li>
+        <li class="bullet-list-padding">Delegated practical tests for new developers in conjunction with the HR team</li>
+        <li class="bullet-list-padding">Led the onboarding and orientation for new developers</li>
+        <li class="bullet-list-padding">Worked with a team to scrape data from an API and use it to populate a client's database</li>
+        <li class="bullet-list-padding">Responsible for working with 3rd party vendors to ensure application expectations are met</li>
+        <li class="bullet-list-padding">Strong understanding of team capabilities, leading to accurate and efficient work assignments</li>
+      </ul>
+
+
+      <div style="margin-bottom: -80px; margin-left: -15px;">
+        <img 
+          src="/src/assets/images/Staghead.png" 
+          alt="Macguyvertech Logo" 
+          width="200" 
+          height="200"
+        >
+      </div>
+      <h4 class="mx-5 pt-5 lighten-text">
+        June 2022 - August 2022 (3 months)
+      </h4>
+      <h3 class="mx-5">
+        Staghead Blockchain Services, Colorado (Remote) - 
+        <i>  
+          Full Stack Engineer / Blockchain Engineer
+        </i>
+      </h3>
+      <ul class="bullet-points mx-5 pt-5">
+        <li class="bullet-list-padding">Spearheaded the development of an intuitive and visually appealing user interface for a groundbreaking blockchain application.</li>
+        <li class="bullet-list-padding">Applied expertise in Solidity to seamlessly integrate and communicate with the Ethereum blockchain, enhancing the app's capabilities and ensuring a robust and secure foundation.</li>
+        <li class="bullet-list-padding">Led the creation of a comprehensive web app mockup, providing a compelling and user-friendly demonstration of the blockchain functionalities to potential clients.</li>
+        <li class="bullet-list-padding">Demonstrated a keen understanding of both front-end design principles and blockchain technology, resulting in a highly engaging and informative user experience.</li>
+      </ul>
+    </v-card>
+  </v-container>
+  <v-container class="center-content">
+    <v-card class="page-card">
+      <h2 class="text-blue mx-5 py-3">
+        Education
+      </h2>
+      <h3 class="mx-5">
+        <b>Dakota State University, South Dakota</b>
+      </h3>
+      <div class="mx-5">
+        <h3>
+          <i>  
+            Bachelor of Computer Science
+          </i>
+        </h3>
+      </div>
+      <h4 class="mx-5 lighten-text">
+        June 2015 - December 2020
+      </h4>
+      <div class="mx-5 pb-5">
+        <a href="https://dsu.edu/" target="_blank" class="custom-link">https://dsu.edu/</a>
+      </div>
+    </v-card>
+  </v-container>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pdfPath: 'http://localhost:5173/' + 'src/assets/downloads/Resume.pdf',
+    };
+  },
+  methods: {
+    openPdfInNewTab() {
+      window.open(this.pdfPath, '_blank');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.download-link {
+  cursor: pointer;
+  color: pink;
+}
+
+.download-link span:hover {
+  color: green;
+}
+
+.bullet-points {
+  list-style-type: disc;
+  padding-left: 20px;
+  padding-bottom: 20px;
+}
+
+@media only screen and (max-width: 600px) {
+  .bullet-points li {
+    text-indent: 20px;
+  }
+}
+
+.bullet-list-padding {
+  padding-bottom: 5px;
+}
+
+.lighten-text {
+  color: #555;
+}
+</style>
